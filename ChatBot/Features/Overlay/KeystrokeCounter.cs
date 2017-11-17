b2f65f0d6cd6ca61184896d47591f57b5e199716
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-11-17 14:53:37 -0800

    Release mode works again.

--- ChatBot/Features/Overlay/KeystrokeCounter.cs
+++ ChatBot/Features/Overlay/KeystrokeCounter.cs
@@ -1,25 +1,19 @@
-﻿// TODO we need to figure out keyboard hooks first
+﻿// TODO needs a working keyboard hook
 
-//using SnagFree.TrayApp.Core;
 //using System;
 
 //namespace HD
 //{
 //  public class KeystrokeCounter : IBotFeature
 //  {
-//    GlobalKeyboardHook _globalKeyboardHook;
-
 //    public static int keystrokeCountSinceLastEvent;
 
 //    void IBotFeature.Init()
 //    {
-//      _globalKeyboardHook = new GlobalKeyboardHook();
-//      _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+//      InterceptKeys.instance.onKeyDown += OnKeyDown;
 //    }
 
-//    void OnKeyPressed(
-//      object sender,
-//      GlobalKeyboardHookEventArgs e)
+//    void OnKeyDown()
 //    {
 //      keystrokeCountSinceLastEvent++;
 //    }
